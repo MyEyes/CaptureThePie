58c3c361e02d41d736c3ebe3af998a5eba669a63
MyEyes <firzen@firzberry3.speedport.ip>
2019-08-21 21:22:24 +0000

    Added small ROP chain challenge

---
+++ level4/level4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "flag.h"
+
+int dummy()
+{
+	asm volatile(
+		"mov r0, r3\n\t"
+		"pop {pc}\n\t"	
+	);
+}
+
+void test()
+{
+	char buffer[0x80];
+	printf("Alright!\nNo more printing of the flag! In fact, I'm so confident, I'll even tell you where it is and where you are now:\n0x%x\nSo can we end this now?\n", FLAG);
+	gets(buffer);
+	if(strcmp(buffer,"Yes")){
+		printf("Still not giving in?\n");
+	}
+	else
+	{
+		printf("Ha!\n");
+	}
+}
+
+int main(int argc, char **argv)
+{
+	test();
+}
