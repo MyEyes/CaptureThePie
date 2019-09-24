633a7eba7d11cf6c20258d1d3e6b9fa5caf593ed
MyEyes <nils.ole.timm@googlemail.com>
2019-09-24 15:02:50 +0000

    Fixed minor bug with level9 run script

--- level9/run.sh
+++ level9/run.sh
@@ -1,1 +1,1 @@
-socat -dd TCP4-LISTEN:9008,fork,reuseaddr EXEC:./a.out,pty,setuid=ctf,echo=0,raw,iexten=0 &
+socat -dd TCP4-LISTEN:9009,fork,reuseaddr EXEC:./a.out,pty,setuid=ctf,echo=0,raw,iexten=0 &
